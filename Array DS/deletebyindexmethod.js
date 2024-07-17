f29e37f58123dabf76ff117fb9a5a57fa6d7e9e1
avinashtandon <avinashtandon07@gmail.com>
2024-07-18 01:40:48 +0530

    Update deletebyindexmethod.js

--- Array DS/deletebyindexmethod.js
+++ Array DS/deletebyindexmethod.js
@@ -14,7 +14,7 @@ class MyArry {
         for(let i=index; i<this.length-1; i++){
             this.data[i] = this.data[i+1]
         }
-        delete this.data[this.length-1];
+        delete this.data[this.length -1];
         this.length--;
         return ele;
     }
