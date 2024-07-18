0f536cc8d77a2a0f08e3584ffa95558810f93c01
avinashtandon <avinashtandon07@gmail.com>
2024-07-19 02:06:22 +0530

    m

--- Array DS/deletebyindexmethod.js
+++ Array DS/deletebyindexmethod.js
@@ -9,7 +9,7 @@ class MyArry {
         this.length++; // Increases the 'length' of the array
         return this.length; // Returns the new length of the array
     }
-    deletebyindex(index){
+    deleteByIndex(index){
         const ele = this.data[index]; // Retrieves the element located at the specified index in the data object.
         for(let i=index; i<this.length-1; i++){
             this.data[i] = this.data[i+1]
@@ -20,13 +20,13 @@ class MyArry {
     }
 }
 
-const mynewarry = new MyArry(); // Creates a new instance of 'MyArry'
-mynewarry.push("apple"); // Adds the string "apple" to 'mynewarry'
-mynewarry.push("orange");
-mynewarry.push("basket");
-mynewarry.push("mangoes");
-mynewarry.push("trees");
+const myNewArray = new MyArry(); // Creates a new instance of 'MyArry'
+myNewArray.push("apple"); // Adds the string "apple" to 'myNewArray'
+myNewArray.push("orange");
+myNewArray.push("basket");
+myNewArray.push("mangoes");
+myNewArray.push("trees");
 
-console.log(mynewarry);
-console.log(mynewarry.deletebyindex(2));
-console.log(mynewarry);
+console.log(myNewArray);
+console.log(myNewArray.deleteByIndex(2));
+console.log(myNewArray);
